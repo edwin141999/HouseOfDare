179877da8bbbd52ba809cbbe0f84c7ab5ee0477b
edwin141999 <183433@ids.upchiapas.edu.mx>
2021-07-22 03:05:12 -0500

    ultimo cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDTeamVwO7s-Of4qnAjCXNIk9oHTf1KGcs",
+    authDomain: "houseofdare2021.firebaseapp.com",
+    projectId: "houseofdare2021",
+    storageBucket: "houseofdare2021.appspot.com",
+    messagingSenderId: "911737900261",
+    appId: "1:911737900261:web:dd7fe6d6fec4f3a190d80c",
+    measurementId: "G-9VRZSG5K7X"
+  }
 };
